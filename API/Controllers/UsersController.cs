a24052cfaf86a57dbb924c3137dfb1645c3a48f7
Daniel Tremura <tremuradaniel@gmail.com>
2023-03-19 14:54:36 +0200

    making async calls

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,34 +1,35 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // api/users
-    public class UsersController: ControllerBase
+  [ApiController]
+  [Route("api/[controller]")] // api/users
+  public class UsersController : ControllerBase
+  {
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context)
-        {
-        _context = context;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
-        {
-            var users = _context.Users.ToList();
-
-            return users;
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
-        {
-            var user = _context.Users.Find(id);
-
-            return user;
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+      var users = await _context.Users.ToListAsync();
+
+      return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+      var user = await _context.Users.FindAsync(id);
+
+      return user;
+    }
+  }
 }
