acb3fe2209e19665e5f9b8a37963f0db2cd2fccd
Daniel Tremura <tremuradaniel@gmail.com>
2023-03-24 06:57:53 +0200

    added validations

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDTO
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string  Password { get; set; }
     }
 }
