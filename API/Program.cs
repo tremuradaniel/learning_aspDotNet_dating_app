7bef31089ff6f014821fe822118e02b7dfe734ab
Daniel Tremura <tremuradaniel@gmail.com>
2023-03-25 10:21:36 +0200

    adding a token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.Entities;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,24 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
+
+// AddTransient -- too short lived for a http request - created and disposed
+// of as and when it's used
+
+// AddSingleton -- create a service when the application first starts and is 
+// never disposed until the application has closed down
+
+// AddScoped -- when we create a controller, as in a request hit the endpoint
+// so the framework instantiates a new instance of that controller,
+// the controller looks at its depencencies or the framework does and determines
+// that it must create these services and create new instances of these services
+// when the controllers are created 
+// WHEN the controller is disposed of at the end of the HTTP request, then
+// any depencennt services are also disposed 
+
+// AddScoped would have worked with TokenService directly, but using the 
+// interface has an advantage when testing - easier to mock stuff
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
