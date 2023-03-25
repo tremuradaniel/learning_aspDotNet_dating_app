37f7ef44284a3566620dff9daf7a895c1f3b57d6
Daniel Tremura <tremuradaniel@gmail.com>
2023-03-25 11:05:46 +0200

    adding the auth middleware

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
   // api/users
+
+  [Authorize]
   public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
@@ -15,6 +18,7 @@ namespace API.Controllers
       _context = context;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
+using System.Text;
 using API.Data;
-using API.Entities;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,10 +38,27 @@ builder.Services.AddCors();
 // interface has an advantage when testing - easier to mock stuff
 builder.Services.AddScoped<ITokenService, TokenService>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])
+      ),
+      ValidateIssuer = false,
+      ValidateAudience = false
+    };
+  });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+// these 2 below must be between cors and MapControllers
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
