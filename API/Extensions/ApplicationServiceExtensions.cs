11e7597b226906e67a1dcbf6591b43d54f54b4b8
Daniel Tremura <tremuradaniel@gmail.com>
2023-03-26 17:01:53 +0300

    adding extension methods

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,42 @@
+using API.Data;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config
+        ) 
+        {
+            services.AddDbContext<DataContext>(opt => 
+            {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors();
+
+            // AddTransient -- too short lived for a http request - created and disposed
+            // of as and when it's used
+
+            // AddSingleton -- create a service when the application first starts and is 
+            // never disposed until the application has closed down
+
+            // AddScoped -- when we create a controller, as in a request hit the endpoint
+            // so the framework instantiates a new instance of that controller,
+            // the controller looks at its depencencies or the framework does and determines
+            // that it must create these services and create new instances of these services
+            // when the controllers are created 
+            // WHEN the controller is disposed of at the end of the HTTP request, then
+            // any depencennt services are also disposed 
+
+            // AddScoped would have worked with TokenService directly, but using the 
+            // interface has an advantage when testing - easier to mock stuff
+            services.AddScoped<ITokenService, Services.TokenService>();
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,4 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,43 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
 
-// AddTransient -- too short lived for a http request - created and disposed
-// of as and when it's used
-
-// AddSingleton -- create a service when the application first starts and is 
-// never disposed until the application has closed down
-
-// AddScoped -- when we create a controller, as in a request hit the endpoint
-// so the framework instantiates a new instance of that controller,
-// the controller looks at its depencencies or the framework does and determines
-// that it must create these services and create new instances of these services
-// when the controllers are created 
-// WHEN the controller is disposed of at the end of the HTTP request, then
-// any depencennt services are also disposed 
-
-// AddScoped would have worked with TokenService directly, but using the 
-// interface has an advantage when testing - easier to mock stuff
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  .AddJwtBearer(options => {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-      ValidateIssuerSigningKey = true,
-      IssuerSigningKey = new SymmetricSecurityKey(
-        Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])
-      ),
-      ValidateIssuer = false,
-      ValidateAudience = false
-    };
-  });
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
