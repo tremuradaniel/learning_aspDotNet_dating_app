e06b56a107acf8964b87aa5668ec728cabdd3be0
Daniel Tremura <tremuradaniel@gmail.com>
2023-04-19 19:33:07 +0300

    adding a member service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/User';
+import { environment } from 'src/environments/environment';
 
 // services are singletons - they're instantiated when our application starts 
 // and destroyed our app shuts down
@@ -11,7 +12,7 @@ import { User } from '../_models/User';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:7092/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
