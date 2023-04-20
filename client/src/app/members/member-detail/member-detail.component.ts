a48defb7c1751f22eba46b6ebf935b847024f35c
Daniel Tremura <tremuradaniel@gmail.com>
2023-04-20 08:53:52 +0300

    routing to the detailed page

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
+import { MembersService } from 'src/app/_services/members.service';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/Member';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member | undefined;
 
-  constructor() { }
+  constructor(
+    private membersService: MembersService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get("username");
+
+    if (!username) {
+      return;
+    }
+
+    this.membersService.getMember(username).subscribe({
+      next: member => this.member = member
+    });
   }
 
 }
