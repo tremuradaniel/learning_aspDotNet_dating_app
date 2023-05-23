b9a38cce2d8f4f82cf43c8f7524655eaf126fe36
Daniel Tremura <tremuradaniel@gmail.com>
2023-05-23 07:32:59 +0300

    custom validators + validation feedback

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from './../_services/account.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -24,14 +24,28 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
         Validators.maxLength(8)
       ]),
-      confirmPassword: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password')
+      ]),
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmedPassword'].updateValueAndValidity()
     })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {
+        notMatching: true
+      };
+    }
   }
 
   register() {
