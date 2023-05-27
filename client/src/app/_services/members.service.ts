4c5136d9d0e928a8131e6850f3975c660087bb25
Daniel Tremura <tremuradaniel@gmail.com>
2023-05-27 10:35:05 +0300

    restoring caching for member detailed

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -59,10 +59,13 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
-    if (member) {
-      return of (member);
-    }
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username)
+    ;
+    
+    if (member) return of(member);
+    
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
