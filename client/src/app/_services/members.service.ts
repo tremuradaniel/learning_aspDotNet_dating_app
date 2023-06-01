3c167e84b2846f6efffea2b00555df4cfcfbe4b2
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-01 17:06:53 +0300

    paginating the likes on the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -118,9 +118,13 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {});
   }
 
-  getLikes(predicate: string)
+  getLikes(predicate: string, pageNumber: number, pageSize: number)
   {
-    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
 
   deletePhoto(photoId: number) {
