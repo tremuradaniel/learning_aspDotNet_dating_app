d69986a1f1fcec9cac55f2e71ecbcde59f94422e
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-01 17:30:20 +0300

    setting up the message repository

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,16 @@
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PageList<MessageDTO>> GetMessagesForUser();
+        Task<IEnumerable<MessageDTO>> GetMessageThread(int currentUserId, int IX_Messages_RecipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
