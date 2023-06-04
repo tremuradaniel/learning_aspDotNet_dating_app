ef6e631960f5ac91a907aa1e328e87d9bdfeb221
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-04 13:06:11 +0300

    adding a message controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,58 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController: BaseApiController
+    {
+    private readonly IUserRepository _userRepository;
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMapper _mapper;
+
+    // api/messages
+    public MessagesController(
+        IUserRepository userRepository, 
+        IMessageRepository messageRepository, 
+        IMapper mapper
+    ) {
+        this._userRepository = userRepository;
+        this._messageRepository = messageRepository;
+        this._mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<MessageDTO>> CreateMessage(CreateMessageDTO createMessageDTO)
+    {
+        var username = User.GetUsername();
+
+        if (username == createMessageDTO.RecipientUsername.ToLower()) {
+            return BadRequest("You cannot send messages to yourself");
+        }
+
+        var sender = await _userRepository.GetUserByUsernameAsync(username);
+
+        var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDTO.RecipientUsername);
+    
+        if (recipient == null) return NotFound();
+
+        var message = new Message
+        {
+            Sender = sender,
+            Recipient = recipient,
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDTO.Content
+        };
+
+        _messageRepository.AddMessage(message);
+
+        if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDTO>(message));
+    
+        return BadRequest("failed to send message");
+    }
+  }
+}
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -44,6 +44,7 @@ namespace API.Extensions
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
             return services;
         }
     }
