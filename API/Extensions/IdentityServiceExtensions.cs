00a9418ed129697164b46ca70d95db53360aee52
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-05 07:12:46 +0300

    configuring the startup class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
@@ -10,6 +13,17 @@ namespace API.Extensions
             this IServiceCollection services, 
             IConfiguration config
         ) {
+            services.AddIdentityCore<AppUser>(
+                opt => {
+                    opt.Password.RequireNonAlphanumeric = false;
+                }
+            )
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>()
+            ;
+
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => {
                 options.TokenValidationParameters = new TokenValidationParameters
