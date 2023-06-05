6847e6800c10499d2794796fbc7cf3380c4f721b
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-05 07:35:40 +0300

    updating the account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
@@ -11,17 +11,17 @@ namespace API.Controllers
     // api/account
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
 
         public AccountController(
-            DataContext context,
+            UserManager<AppUser> userManager,
             ITokenService tokenService,
             IMapper mapper
         )
         {
-                _context = context;
+                _userManager = userManager;
                 _tokenService = tokenService;
                 _mapper = mapper;
         }
@@ -37,8 +37,9 @@ namespace API.Controllers
             var user = _mapper.Map<AppUser>(registerDTO);
             user.UserName = registerDTO.Username.ToLower();
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
             return new UserDTO{
                 Username = user.UserName,
@@ -51,7 +52,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users
+            var user = await _userManager.Users
                 // the entity framework will not load related entities (Photos)
                 // by default - in order to use them, we must eager load them
                 // with Include
@@ -60,6 +61,10 @@ namespace API.Controllers
                 x.UserName == loginDTO.Username
             );
             if (user == null) return Unauthorized("Invalid username");
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDTO.Password);
+
+            if (!result) return Unauthorized("Invalid password");
 
             return new UserDTO{
                 Username = user.UserName,
@@ -72,7 +77,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
