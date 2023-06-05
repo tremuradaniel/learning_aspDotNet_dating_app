95110613946f24abdd4ed21725c9570708e6ce02
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-05 09:51:54 +0300

    adding a custom directive

---
+++ client/src/app/_directives/has-role.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { User } from '../_models/User';
+import { AccountService } from '../_services/account.service';
+import { take } from 'rxjs';
+
+@Directive({
+  selector: '[appHasRole]' // *appHasRole='["Admin"]'
+})
+export class HasRoleDirective {
+
+  @Input() appHasRole: string[] = [];
+  user: User = {} as User;
+
+  constructor(
+    private viewContainerRef: ViewContainerRef, 
+    private templateRef: TemplateRef<any>,
+    private accountService: AccountService
+  ) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe({
+        next: user => {
+          if (user) this.user = user
+        }
+      })
+     }
+
+  ngOnInit(): void {
+    if (this.user.roles.some(r => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear()
+    }
+  }
+
+}
