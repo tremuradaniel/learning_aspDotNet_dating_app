2e1cf068779018c5115c409c5e710ffd188a0097
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-05 07:00:32 +0300

    setting up the entities for identity and role management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,3 @@
-using System.Security.Cryptography;
-using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -37,11 +35,7 @@ namespace API.Controllers
             if (await UserExists(registerDTO.Username)) return BadRequest("Username is taken");
             
             var user = _mapper.Map<AppUser>(registerDTO);
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDTO.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -66,16 +60,6 @@ namespace API.Controllers
                 x.UserName == loginDTO.Username
             );
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
-
-            for (int i = 0; i < computedHash.Length; i++) {
-                if (computedHash[i] != user.PasswordHash[i]) {
-                    return Unauthorized("Invalid password");
-                }
-            }
 
             return new UserDTO{
                 Username = user.UserName,
