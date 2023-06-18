9d96461be364a0ac1a5bef2249f53cf8c166d342
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-18 13:23:11 +0300

    creating a messaging hub

--- API/Program.cs
+++ API/Program.cs
@@ -37,6 +37,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapHub<PresenceHub>("hubs/presence");
+app.MapHub<MessageHub>("hubs/message");
 
 using var scope = app.Services.CreateScope();
 
