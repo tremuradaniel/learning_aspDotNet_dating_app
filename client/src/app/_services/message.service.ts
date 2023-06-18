5c7037363064b4f58ee195c67109867ce176020e
Daniel Tremura <tremuradaniel@gmail.com>
2023-06-18 13:40:32 +0300

    adding the hub connection to the message service

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -3,18 +3,47 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 import { Message } from '../_models/Message';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { User } from '../_models/User';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  private hubConnection?: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
+
   sendMessage(username: string, content: string) {
     return this.http.post<Message>(this.baseUrl + 'messages', 
       {recipientUsername: username, content});
   }
   baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
 
   constructor(private http: HttpClient) { }
+
+  createHubConnection(user: User, otherUsername: string)
+  {
+    this.hubConnection = new HubConnectionBuilder()
+    .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
+      accessTokenFactory: () => user.token
+    }).withAutomaticReconnect()
+      .build()
+    ;
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    this.hubConnection.on("ReceivedMessageThread", messages => {
+      this.messageThreadSource.next(messages);
+    });
+  }
+
+  stopHubConnection()
+  {
+    this.hubConnection?.stop();
+  }
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = getPaginationHeaders(pageNumber, pageSize);
